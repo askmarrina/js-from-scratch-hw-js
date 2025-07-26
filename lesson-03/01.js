0a0384bcc886a75b577de5febc7495d6dc30a64d
Marina K <askmarrina@gmail.com>
2025-07-26 18:39:31 +0200

    tasks from lesson 03 are completed

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(a, b) {
+    let average = (a+b)/2
+    return average;
+}
+
+console.log(calculateAverage(2, 3))
--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,13 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(a) {
+    if(a % 2 === 1) {
+        console.log('число нечетное')
+    } else {
+        console.log('число четное')
+    }
+    return a;
+}
+
+console.log(isEven(10))
