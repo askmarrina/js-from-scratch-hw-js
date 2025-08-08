0cd77d45501c228c6c8e8cb4e1a6eced1060e054
Marina K <askmarrina@gmail.com>
2025-08-08 14:55:57 +0200

    completed 1 task in lesson 08

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -7,7 +7,43 @@
 */
 
 function capitalizeWords(string) {
-
+    const words = string.split(' ');
+    capitalizeWords = words.map(word => {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    // console.log(capitalizeWords);
+    let result = capitalizeWords.join(' ');
+    return result;
 }
+console.log(capitalizeWords('heLlo wOrld frOm javasCript'))
+
+
+
+
+// console.log(capitalizeWords('hello world from javascript'))
+//
+// const string = 'hello world from javascript';
+// const words = string.split(' ');
+//
+// const capitalizeWords = words.map(word => {
+//     return word[0].toUpperCase() + word.slice(1).toLowerCase();
+// })
+// console.log(capitalizeWords);
+// let result = capitalizeWords.join(' ');
+// console.log(result);
+
+
+
+
+
+// for (let i = 0; i < words.length; i++) {
+//     let word = words[i];
+//     // console.log(word);
+//     // console.log(word[0])
+//     let capitalizeWord = word[0].toUpperCase()+word.slice(1)
+//     // console.log(capitalizeWord);
+//     // let result = capitalizeWord.join(" ")
+//     // console.log(result)
+// }
 
 
